bef51783e59875d8bf6c5e31f15354695582e272
Mrunal <mrunaljoshi11@yahoo.com>
2020-12-11 19:47:03 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -52,11 +52,14 @@ app.get('/weather',(req,res)=>{
         })
     }
     geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        
         if(error){
             return res.send({error})
         }
 
         forecast(longitude,latitude,(error,forecastdata)=>{
+            console.log(latitude)
+            console.log(longitude)
             if(error){
                 return res.send({error})
             }
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (longitude,latitude,callback) =>{
-    const url = 'http://api.weatherstack.com/current?access_key=cec95c73b5df8dcaf3609d308ac217c7&query=' + longitude + ',' + latitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=cec95c73b5df8dcaf3609d308ac217c7&query=' + latitude + ',' + longitude + '&units=f'
 
     request({url,json:true},(error,{body})=>{
         if(error){
@@ -9,7 +9,7 @@ const forecast = (longitude,latitude,callback) =>{
         }else if(body.error){
             callback('Undefined location',undefined)
         }else{
-            callback(undefined,body.current.weather_descriptions[0] + ".It is currently " + body.current.temperature + " degress out.But it feels like " + body.current.feelslike)
+            callback(undefined,body.current.weather_descriptions[0] + ".It is currently " + body.current.temperature + " degrees Farenheit out.But it feels like " + body.current.feelslike + " Farenheit.The humidity is " + body.current.humidity + "%")
         }
     })
 }
